c2afe0ad055668aaca99fc7cc47bb2c1edbf764c
CeaserZhao <chkevin.zhao@gmail.com>
2026-02-12 18:22:40 +0800

    Update ScanProbe.cpp

--- src/ScanProbe.cpp
+++ src/ScanProbe.cpp
@@ -4,4 +4,15 @@
 
 // 设置空间网格
 void ScanProbe::setSpatialGrid(const SpatialGrid& grid) {
-    this
+    this->spatialGrid = grid;
+}
+
+// 设置密度阈值
+void ScanProbe::setDensityThreshold(float threshold) {
+    this->densityThreshold = threshold;
+}
+
+// 计算局部密度最大值
+Vector3 ScanProbe::findLocalDensityMax(const Vector3& startPosition, float searchRadius) const {
+    Vector3 bestPosition = startPosition;
+    float bestDensity
